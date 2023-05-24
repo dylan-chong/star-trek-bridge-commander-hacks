b2b44bc8536ff74e21dda37bbaa5f31b8cdeeef6
Dylan Chong <dylan.chong@icloud.com>
2023-05-24 17:19:58 +1200

    [Original] Files needed to add new ships

---
+++ scripts/Icons/ShipIcons.py
@@ -0,0 +1,149 @@
+# ShipIcons.py
+#
+# Load Ship Icons for interface.
+#
+#
+
+import App
+import Foundation
+import Registry
+
+shipIconNames = {}
+shipIconNums = {}
+topSpecies = 0
+
+class ShipIconDef:
+	def __init__(self, name, dict = { 'x': 128, 'y': 128 }):
+		global topSpecies, shipIcons
+		self.name = name
+		self.__dict__.update(dict)
+
+		if not self.__dict__.has_key('file'):
+			self.file = 'Data/Icons/Ships/' + name + '.tga'
+
+		if self.__dict__.has_key('species'):
+			if self.species > topSpecies:	topSpecies = self.species
+		else:
+			self.species = topSpecies + 1
+			topSpecies = self.species
+
+		shipIconNames[name] = self
+		shipIconNums[self.species] = self
+
+# Function to load LCARS icon group
+def LoadShipIcons(ShipIcons = None):
+
+	if ShipIcons is None:
+		ShipIcons = App.g_kIconManager.CreateIconGroup("ShipIcons")
+		# Add LCARS icon group to IconManager
+		App.g_kIconManager.AddIconGroup(ShipIcons)
+
+	# Glass for when no ship is selected
+	TextureHandle = ShipIcons.LoadIconTexture('Data/Icons/Bridge/Background/ScreenBlock.tga')
+	ShipIcons.SetIconLocation(App.SPECIES_UNKNOWN, TextureHandle, 0, 0, 8, 8)
+
+	ShipIconDef('Sabre', { 'species': 109 } )
+	ShipIconDef('Miranda', { 'species': 110 } )
+	ShipIconDef('Nova', { 'species': 111 } )
+	ShipIconDef('Norway', { 'species': 112 } )
+	ShipIconDef('Steamrunner', { 'species': 113 } )
+	ShipIconDef('Constellation', { 'species': 114 } )
+	ShipIconDef('Centaur', { 'species': 115 } )
+	ShipIconDef('Defiant', { 'species': 116 } )
+	ShipIconDef('Intrepid', { 'species': 117 } )
+	ShipIconDef('Excelsior', { 'species': 118 } )
+	ShipIconDef('ExcelsiorR', { 'species': 119 } )
+	ShipIconDef('Ambassador', { 'species': App.SPECIES_AMBASSADOR } )
+	ShipIconDef('Luna', { 'species': 120 } )
+	ShipIconDef('Akira', { 'species': App.SPECIES_AKIRA } )
+	ShipIconDef('Nebula', { 'species': App.SPECIES_NEBULA } )
+	ShipIconDef('Galaxy', { 'species': App.SPECIES_GALAXY } )
+	ShipIconDef('Sovereign', { 'species': App.SPECIES_SOVEREIGN } )
+	ShipIconDef('Prometheus', { 'species': 121} )
+	ShipIconDef('SovereignR', { 'species': 122 } )
+	ShipIconDef('BirdOfPrey', { 'species': App.SPECIES_BIRD_OF_PREY } )
+	ShipIconDef('Kvort', { 'species': 403 } )
+	ShipIconDef('KTinga', { 'species': 404 } )
+	ShipIconDef('Vorcha', { 'species': App.SPECIES_VORCHA } )
+	ShipIconDef('Neghvar', { 'species': 405 } )
+	ShipIconDef('Talon', { 'species': 302 } )
+	ShipIconDef('Falcon', { 'species': 303 } )
+	ShipIconDef('Valdore', { 'species': 304 } )
+	ShipIconDef('Warbird', { 'species': App.SPECIES_WARBIRD } )
+	ShipIconDef('Scimitar', { 'species': 305 } )
+	ShipIconDef('Hideki', { 'species': 205 } )
+	ShipIconDef('Galor', { 'species': App.SPECIES_GALOR } )
+	ShipIconDef('Keldon', { 'species': App.SPECIES_KELDON } )
+	ShipIconDef('ObsidianKeldon', { 'species': 206 } )
+	ShipIconDef('Hybrid', { 'species': App.SPECIES_CARDHYBRID } )
+	ShipIconDef('Bug', { 'species': 801 } )
+	ShipIconDef('Breen', { 'species': 802 } )
+	ShipIconDef('Vorta', { 'species': 803 } )
+	ShipIconDef('Founder', { 'species': 804 } )
+	ShipIconDef('Rectangle', { 'species': 901 } )
+	ShipIconDef('Diamond', { 'species': 902 } )
+	ShipIconDef('Sphere', { 'species': 903 } )
+	ShipIconDef('Cube', { 'species': 904 } )
+	ShipIconDef('CubeT', { 'species': 905 } )
+	ShipIconDef('CubeA', { 'species': 906 } )
+	ShipIconDef('Marauder', { 'species': App.SPECIES_MARAUDER } )
+	ShipIconDef('KessokLight', { 'species': App.SPECIES_KESSOK_LIGHT } )
+	ShipIconDef('KessokHeavy', { 'species': App.SPECIES_KESSOK_HEAVY } )
+	ShipIconDef('SonaCruiser', { 'species': 805 } )
+	ShipIconDef('SonaBattleship', { 'species': 806 } )
+	ShipIconDef('Bioship', { 'species': 750 } )
+	ShipIconDef('HirogenHunter', { 'species': 751 } )
+	ShipIconDef('HirogenHoloship', { 'species': 752 } )
+	ShipIconDef('HirogenVenatic', { 'species': 753 } )
+	ShipIconDef('KazonRaider', { 'species': 754 } )
+	ShipIconDef('KazonPredator', { 'species': 755 } )
+	ShipIconDef('KrenimPatrol', { 'species': 756 } )
+	ShipIconDef('KrenimWarship', { 'species': 757 } )
+	ShipIconDef('KrenimTimeship', { 'species': 758 } )
+	ShipIconDef('MalonFreighter', { 'species': 759 } )
+	ShipIconDef('VidiianCruiser', { 'species': 760 } )
+	ShipIconDef('FedShuttle', { 'species': App.SPECIES_SHUTTLE } )
+	ShipIconDef('Fighter', { 'species': 123 } )
+	ShipIconDef('Danube', { 'species': 124 } )
+	ShipIconDef('DanubeR', { 'species': 125 } )
+	ShipIconDef('DeltaFlyer', { 'species': 126 } )
+	ShipIconDef('ArmoredIntrepid', { 'species': 127 } )
+	ShipIconDef('NX', { 'species': 128 } )
+	ShipIconDef('Constitution', { 'species': 129 } )
+	ShipIconDef('ConstitutionR', { 'species': 130 } )
+	ShipIconDef('Freighter', { 'species': App.SPECIES_FREIGHTER } )
+	ShipIconDef('Transport', { 'species': App.SPECIES_TRANSPORT } )
+	ShipIconDef('LifeBoat', { 'species': App.SPECIES_ESCAPEPOD } )
+	ShipIconDef('CardFreighter', { 'species': App.SPECIES_CARDFREIGHTER } )
+	ShipIconDef('Sunbuster', { 'species': App.SPECIES_SUNBUSTER } )
+	ShipIconDef('OrbitalWeaponsPlatform', { 'species': 207 } )
+	ShipIconDef('KessokMine', { 'species': App.SPECIES_KESSOKMINE } )
+	ShipIconDef('FedOutpost', { 'species': App.SPECIES_FED_OUTPOST } )
+	ShipIconDef('SpaceFacility', { 'species': App.SPECIES_SPACE_FACILITY } )
+	ShipIconDef('FedComplex', { 'species': 761 } )
+	ShipIconDef('FedStarbase', { 'species': App.SPECIES_FED_STARBASE } )
+	ShipIconDef('DS9', { 'species': 762 } )
+	ShipIconDef('CardOutpost', { 'species': App.SPECIES_CARD_OUTPOST } )
+	ShipIconDef('CardStation', { 'species': App.SPECIES_CARD_STATION } )
+	ShipIconDef('CardStarbase', { 'species': App.SPECIES_CARD_STARBASE } )
+	ShipIconDef('CommArray', { 'species': App.SPECIES_COMMARRAY } )
+	ShipIconDef('CommLight', { 'species': App.SPECIES_COMMLIGHT } )
+	ShipIconDef('DryDock', { 'species': App.SPECIES_DRYDOCK } )
+	ShipIconDef('Probe', { 'species': App.SPECIES_PROBE } )
+	ShipIconDef('ProbeType2', { 'species': App.SPECIES_PROBETYPE2 } )
+	ShipIconDef('Asteroid', { 'species': App.SPECIES_ASTEROID } )
+
+	global topSpecies
+	topSpecies = topSpecies + 100
+
+	for shipDef in Foundation.shipList._keyList.values():
+		if shipIconNames.has_key(shipDef.iconName):
+			iconDef = shipIconNames[shipDef.iconName]
+		else:
+			iconDef = ShipIconDef(shipDef.iconName)
+		shipDef.species = iconDef.species
+
+	for i in shipIconNums.values():
+		# print i.file, i.species, ' : ',
+		TextureHandle = ShipIcons.LoadIconTexture(i.file)
+		ShipIcons.SetIconLocation(i.species, TextureHandle, 0, 0, 128, 128)
---
+++ scripts/Multiplayer/SpeciesToShip.py
@@ -0,0 +1,316 @@
+import App
+
+# types for initializing objects create from C.
+UNKNOWN = 0
+SABRE = 1
+MIRANDA = 2
+NOVA = 3
+NORWAY = 4
+STEAMRUNNER = 5
+CONSTELLATION = 6
+CENTAUR = 7
+DEFIANT = 8
+INTREPID = 9
+EXCELSIOR = 10
+EXCELSIORREFIT = 11
+AMBASSADOR = 12
+LUNA = 13
+AKIRA = 14
+NEBULA = 15
+GALAXY = 16
+SOVEREIGN = 17
+PROMETHEUS = 18
+SOVEREIGNREFIT = 19
+BIRDOFPREY = 20
+KVORT = 21
+KTINGA = 22
+VORCHA = 23
+NEGHVAR = 24
+TALON = 25
+FALCON = 26
+VALDORE = 27
+WARBIRD = 28
+SCIMITAR = 29
+HIDEKI = 30
+GALOR = 31
+KELDON = 32
+OBSIDIANKELDON = 33
+CARDHYBRID = 34
+BUG = 35
+BREEN = 36
+VORTA = 37
+FOUNDER = 38
+RECTANGLE = 39
+DIAMOND = 40
+SPHERE = 41
+CUBE = 42
+TACTICALCUBE = 43
+ASSIMILATIONCUBE = 44
+MARAUDER = 45
+KESSOKLIGHT = 46
+KESSOKHEAVY = 47
+SONACRUISER = 48
+SONABATTLESHIP = 49
+BIOSHIP = 50
+HIROGENHUNTER = 51
+HIROGENHOLOSHIP = 52
+HIROGENVENATIC = 53
+KAZONRAIDER = 54
+KAZONPREDATOR = 55
+KRENIMPATROL = 56
+KRENIMWARSHIP = 57
+KRENIMTIMESHIP = 58
+MALONFREIGHTER = 59
+VIDIIANCRUISER = 60
+SHUTTLE = 61
+FIGHTER = 62
+DANUBE = 63
+DANUBEREFIT = 64
+DELTAFLYER = 65
+ARMOREDINTREPID = 66
+NX = 67
+CONSTITUTION = 68
+CONSTITUTIONREFIT = 69
+FREIGHTER = 70
+TRANSPORT = 71
+ESCAPEPOD = 72
+CARDFREIGHTER = 73
+SUNBUSTER = 74
+WEAPONSPLATFORM = 75
+KESSOKMINE = 76
+FEDOUTPOST = 77
+SPACEFACILITY = 78
+FEDCOMPLEX = 79
+FEDSTARBASE = 80
+DS9 = 81
+CARDOUTPOST = 82
+CARDSTATION = 83
+CARDSTARBASE = 84
+COMMARRAY = 85
+COMMLIGHT = 86
+DRYDOCK = 87
+PROBE = 88
+DECOY = 89
+ASTEROID = 90
+ASTEROID1 = 91
+ASTEROID2 = 92
+ASTEROID3 = 93
+AMAGON = 94
+BIRANUSTATION = 95
+ENTERPRISE = 96
+GERONIMO = 97
+PEREGRINE = 98
+ASTEROIDH1 = 99
+ASTEROIDH2 = 100
+ASTEROIDH3 = 101
+MAX_SHIPS = 102
+MAX_FLYABLE_SHIPS = 85
+
+# Setup tuples
+kSpeciesTuple = (
+	(None, 0, "Neutral", 0),
+	("Sabre", 109, "Federation", 1),
+	("Miranda", 110, "Federation", 1),
+	("Nova", 111, "Federation", 1),
+	("Norway", 112, "Federation", 1),
+	("Steamrunner", 113, "Federation", 1),
+	("Constellation", 114, "Federation", 1),
+	("Centaur", 115, "Federation", 1),
+	("Defiant", 116, "Federation", 1),
+	("Intrepid", 117, "Federation", 1),
+	("Excelsior", 118, "Federation", 1),
+	("ExcelsiorR", 119, "Federation", 1),
+	("Ambassador", App.SPECIES_AMBASSADOR, "Federation", 1),
+	("Luna", 120, "Federation", 1),
+	("Akira", App.SPECIES_AKIRA, "Federation", 1),
+	("Nebula" , App.SPECIES_NEBULA, "Federation", 1),
+	("Galaxy", App.SPECIES_GALAXY, "Federation", 1),
+	("Sovereign" , App.SPECIES_SOVEREIGN, "Federation", 1),
+	("Prometheus" , 121, "Federation", 1),
+	("SovereignR" , 122, "Federation", 1),
+	("BirdOfPrey", App.SPECIES_BIRD_OF_PREY, "Klingon", 1),
+	("Kvort", 403, "Klingon", 1),
+	("KTinga", 404, "Klingon", 1),
+	("Vorcha" , App.SPECIES_VORCHA, "Klingon", 1),
+	("Neghvar", 405, "Klingon", 1),
+	("Talon" , 302, "Romulan", 1),
+	("Falcon" , 303, "Romulan", 1),
+	("Valdore" , 304, "Romulan", 1),
+	("Warbird" , App.SPECIES_WARBIRD, "Romulan", 1),
+	("Scimitar" , 305, "Romulan", 1),
+	("Hideki" , 205, "Cardassian", 1),
+	("Galor" , App.SPECIES_GALOR, "Cardassian", 1),
+	("Keldon" , App.SPECIES_KELDON, "Cardassian", 1),
+	("ObsidianKeldon" , 206, "Cardassian", 1),
+	("CardHybrid", App.SPECIES_CARDHYBRID, "Cardassian", 1),
+	("Bug" , 801, "Dominion", 1),
+	("Breen" , 802, "Dominion", 1),
+	("Vorta" , 803, "Dominion", 1),
+	("Founder" , 804, "Dominion", 1),
+	("Rectangle" , 901, "Borg", 1),
+	("Diamond" , 902, "Borg", 1),
+	("Sphere" , 903, "Borg", 1),
+	("Cube" , 904, "Borg", 1),
+	("CubeT" , 905, "Borg", 1),
+	("CubeA" , 906, "Borg", 1),
+	("Marauder" , App.SPECIES_MARAUDER, "Ferengi", 1),
+	("KessokLight" , App.SPECIES_KESSOK_LIGHT, "Kessok", 1),
+	("KessokHeavy" , App.SPECIES_KESSOK_HEAVY, "Kessok", 1),  
+	("SonaCruiser" , 805, "Sona", 1),
+	("SonaBattleship" , 806, "Sona", 1),
+	("Bioship" , 750, "Delta Quadrant", 1),
+	("HirogenHunter" , 751, "Delta Quadrant", 1),
+	("HirogenHoloship" , 752, "Delta Quadrant", 1),
+	("HirogenVenatic" , 753, "Delta Quadrant", 1),
+	("KazonRaider" , 754, "Delta Quadrant", 1),
+	("KazonPredator" , 755, "Delta Quadrant", 1),
+	("KrenimPatrol" , 756, "Delta Quadrant", 1),
+	("KrenimWarship" , 757, "Delta Quadrant", 1),
+	("KrenimTimeship" , 758, "Delta Quadrant", 1),
+	("MalonFreighter" , 759, "Delta Quadrant", 1),
+	("VidiianCruiser" , 760, "Delta Quadrant", 1),
+	("Shuttle" , App.SPECIES_SHUTTLE, "Shuttles", 1),
+	("Fighter" , 123, "Shuttles", 1),
+	("Danube" , 124, "Shuttles", 1),
+	("DanubeR" , 125, "Shuttles", 1),
+	("DeltaFlyer" , 126, "Shuttles", 1),
+	("ArmoredIntrepid" , 127, "Other", 1),
+	("NX" , 128, "Other", 1),
+	("Constitution" , 129, "Other", 1),
+	("ConstitutionR" , 130, "Other", 1),
+	("Freighter" , App.SPECIES_FREIGHTER, "Other", 1),
+	("Transport" , App.SPECIES_TRANSPORT, "Other", 1),
+	("EscapePod" , App.SPECIES_ESCAPEPOD, "Other", 1),
+	("CardFreighter" ,  App.SPECIES_CARDFREIGHTER, "Other", 1),
+	("Sunbuster" , App.SPECIES_SUNBUSTER, "Other", 1),
+	("OrbitalWeaponsPlatform" , 207, "Other", 1),
+	("KessokMine" , App.SPECIES_KESSOKMINE, "Other", 1),
+	("FedOutpost" , App.SPECIES_FED_OUTPOST, "Bases", 1),
+	("SpaceFacility" , App.SPECIES_SPACE_FACILITY, "Bases", 1),
+	("FedComplex" , 761, "Bases", 1),
+	("FedStarbase" , App.SPECIES_FED_STARBASE, "Bases", 1),
+	("DS9" , 762, "Bases", 1),
+	("CardOutpost" , App.SPECIES_CARD_OUTPOST, "Bases", 1),
+	("CardStation" , App.SPECIES_CARD_STATION, "Bases", 1),
+	("CardStarbase" , App.SPECIES_CARD_STARBASE, "Bases", 1),
+	("CommArray" , App.SPECIES_COMMARRAY, "Other", 1),
+	("CommLight", App.SPECIES_COMMLIGHT, "Other", 1),
+	("DryDock" , App.SPECIES_DRYDOCK, "Bases", 1),
+	("Probe" , App.SPECIES_PROBE, "Other", 1),
+	("Decoy" , App.SPECIES_PROBETYPE2, "Other", 1),
+	("Asteroid" , App.SPECIES_ASTEROID, "Neutral", 1),
+	("Asteroid1" , App.SPECIES_ASTEROID, "Neutral", 1),
+	("Asteroid2" , App.SPECIES_ASTEROID, "Neutral", 1),
+	("Asteroid3" , App.SPECIES_ASTEROID, "Neutral", 1),
+	("Amagon", App.SPECIES_ASTEROID, "Neutral", 1),
+	("BiranuStation", App.SPECIES_SPACE_FACILITY, "Neutral", 1),
+	("Enterprise", App.SPECIES_SOVEREIGN, "Neutral", 1),
+	("Geronimo", App.SPECIES_AKIRA, "Neutral", 1),
+	("Peregrine", App.SPECIES_NEBULA, "Neutral", 1),
+	("Asteroidh1" , App.SPECIES_ASTEROID, "Neutral", 1),
+	("Asteroidh2" , App.SPECIES_ASTEROID, "Neutral", 1),
+	("Asteroidh3" , App.SPECIES_ASTEROID, "Neutral", 1),
+	(None, 0, "Neutral", 1))
+
+def GetShipFromSpecies (iSpecies):
+	if (iSpecies <= 0 or iSpecies >= MAX_SHIPS):
+#		print ("Species out of range")
+		return None
+
+	pSpecTuple = kSpeciesTuple [iSpecies]
+	pcScript = pSpecTuple [0]
+
+	ShipScript = __import__("ships." + pcScript)
+	ShipScript.LoadModel ()
+	return ShipScript.GetShipStats ()
+
+def GetScriptFromSpecies (iSpecies):
+	if (iSpecies <= 0 or iSpecies >= MAX_SHIPS):
+		return None
+
+	pSpecTuple = kSpeciesTuple [iSpecies]
+	return pSpecTuple [0]
+
+	
+# This function is called from code to fill in the spec of
+# an object that has been serialized over the net.
+def InitObject (self, iType):
+	kStats = GetShipFromSpecies (iType)
+	if (kStats == None):
+		# Failed.  Unknown type. Bail.
+		return 0
+
+	# Now that we have the stats, initialize the objects.
+	# Initialize the ship's model.
+	self.SetupModel (kStats['Name'])
+
+	# Load hardpoints.
+	pPropertySet = self.GetPropertySet()
+	mod = __import__("ships.Hardpoints." + kStats['HardpointFile'])
+
+	App.g_kModelPropertyManager.ClearLocalTemplates ()
+	reload (mod)
+
+	mod.LoadPropertySet(pPropertySet)
+
+	self.SetupProperties()
+
+	self.UpdateNodeOnly()
+		
+	return 1
+
+def CreateShip (iType):
+	# Get ship stats
+	kStats = GetShipFromSpecies (iType)
+
+	if (kStats == None):
+		# Failed.  Unknown type. Bail.
+		return None
+
+#	print ("Creating " + kStats['Name'] + "\n")
+	pShip = App.ShipClass_Create (kStats['Name'])
+
+	sModule = "ships." + kSpeciesTuple [iType][0]
+#	print ("*** Setting script module " + sModule)
+	pShip.SetScript(sModule)
+
+	# Load hardpoints.
+	pPropertySet = pShip.GetPropertySet()
+	mod = __import__("ships.Hardpoints." + kStats['HardpointFile'])
+
+	App.g_kModelPropertyManager.ClearLocalTemplates ()
+	reload(mod)
+
+	mod.LoadPropertySet(pPropertySet)
+
+	pShip.SetupProperties()
+
+	pShip.UpdateNodeOnly()
+		
+	pShip.SetNetType (iType)
+
+	return pShip
+
+def GetNameFromSpecies (iSpecies):
+	pSpecTuple = kSpeciesTuple [iSpecies]
+	pcName = pSpecTuple [0]
+
+	return pcName
+
+def GetIconNum (iSpecies):
+	pSpecTuple = kSpeciesTuple [iSpecies]
+	iNum = pSpecTuple [1]
+
+	return iNum
+
+def GetSideFromSpecies (iSpecies):
+	pSpecTuple = kSpeciesTuple [iSpecies]
+	pcSide = pSpecTuple [2]
+
+	return pcSide
+
+def GetClassFromSpecies (iSpecies):
+	pSpecTuple = kSpeciesTuple [iSpecies]
+	iClass = pSpecTuple [3]
+
+	return iClass
