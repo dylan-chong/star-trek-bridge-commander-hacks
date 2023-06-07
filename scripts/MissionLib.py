b4787a3e5793ceb6e40190560286e970ca5f4ad3
Dylan Chong <dylan.chong@icloud.com>
2023-06-07 21:30:53 +1200

    Change default ship

--- scripts/MissionLib.py
+++ scripts/MissionLib.py
@@ -541,7 +541,7 @@ def CreatePlayerShip(sShipClass, pSet, pcName, sWaypoint, bUnloadShip = 0):
 		import time
 		ships = ['BugRammer', 'Defiant', 'Akira', 'Prometheus', 'Scimitar', 'Shuttle', 'Nova', 'Valdore']
 		sShipClass = ships[int(time.clock()) % len(ships)]
-		sShipClass = 'Scimitar'
+		sShipClass = 'Shuttle'
 	
 	pGame = App.Game_GetCurrentGame()
 
